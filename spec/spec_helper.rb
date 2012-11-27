93e100bff6e114c4de9c7192d0768b835aad5723
John Norman <john@7fff.com>
2012-11-26 19:21:00 -0600

    Tweaks to run on both 1.8.7 and 1.9.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,13 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = 'fixtures/cassettes'
   c.hook_into :fakeweb
+  c.default_cassette_options = { :serialize_with => :json }
+  c.preserve_exact_body_bytes do |http_message|
+    RUBY_VERSION == '1.8.7' || (http_message.body.encoding.name == 'ASCII-8BIT' || !http_message.body.valid_encoding?)
+  end
+  c.filter_sensitive_data("<BOX_CLI_USER>")     { ENV['BOX_CLI_USER'] }
+  c.filter_sensitive_data("<BOX_CLI_PASSWORD>") { ENV['BOX_CLI_PASSWORD'] }
+  c.filter_sensitive_data("<BOX_CLI_API_KEY>")  { ENV['BOX_CLI_API_KEY'] }
   c.configure_rspec_metadata!
 end
 
